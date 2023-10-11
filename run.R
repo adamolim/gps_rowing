8e63effeae4df46c4e9e5518ad047bb854afc9b9
adamolim <adamolim@gmail.com>
2023-10-11 11:29:25 +0200

    first results

---
+++ run.R
@@ -0,0 +1,16 @@
+# Clean console and environnement
+
+
+rm(list=ls())
+
+# Fonction to source the rmd file
+
+runAllChunks <- function(rmd, envir=globalenv()){
+    tempR <- tempfile(tmpdir = ".", fileext = ".R")
+    on.exit(unlink(tempR))
+    knitr::purl(rmd, output=tempR)
+    sys.source(tempR, envir=envir)
+}
+
+runAllChunks("map.Rmd")
+
